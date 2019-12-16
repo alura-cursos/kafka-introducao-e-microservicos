7f610f1b6528718b5d4da92deb07216501a069f6
Caio César Mendonça Souza <caiocesar.msouza@gmail.com>
2019-12-16 19:59:56 -0300

    aula5

--- video4.3/ecommerce/src/main/java/br/com/alura/ecommerce/ConsumerFunction.java
+++
@@ -1,7 +0,0 @@
-package br.com.alura.ecommerce;
-
-import org.apache.kafka.clients.consumer.ConsumerRecord;
-
-public interface ConsumerFunction {
-    void consume(ConsumerRecord<String, String> record);
-}
